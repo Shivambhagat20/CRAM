d9abe200637f7729874028a18ddc0e1d587546fd
Iezzi-S <iezzis@myumanitoba.ca>
2026-04-16 20:33:45 -0500

    fix: broken tests

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,12 +1,12 @@
-import js from "@eslint/js";
-import globals from "globals";
+const js = require("@eslint/js");
+const globals = require("globals");
 
-export default [
+module.exports = [
   js.configs.recommended,
   {
     languageOptions: {
       ecmaVersion: "latest",
-      sourceType: "module",  
+      sourceType: "commonjs",  
       globals: {
         ...globals.node,
       }
--- backend/tests/integration/dbstart.js
+++ backend/tests/integration/dbstart.js
@@ -1,27 +1,29 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
-export async function connect() {
+module.exports = {
+    connect: async function() {
+        if (mongoose.connection.readyState !== 0) {
+            return;
+        }
 
-   if (mongoose.connection.readyState !== 0) {
-        return; 
+        const workerId = process.env.STRYKER_MUTATOR_WORKER || 0;
+        const dbName = `test_db_${workerId}`;
+
+        const localUrl = `mongodb://127.0.0.1:27017/${dbName}`;
+
+        try {
+            await mongoose.connect(localUrl, {
+                serverSelectionTimeoutMS: 5000
+            });
+        } catch (err) {
+            process.exit(1);
+        }
+    },
+
+    disconnect: async function() {
+        if (mongoose.connection.readyState !== 0) {
+            await mongoose.connection.close(true);
+        }
     }
-
-    const workerId = process.env.STRYKER_MUTATOR_WORKER || 0;
-    const dbName = `test_db_${workerId}`;
-    
-    const localUrl = `mongodb://127.0.0.1:27017/${dbName}`;
-    
-    try {
-        await mongoose.connect(localUrl, {
-            serverSelectionTimeoutMS: 5000 
-        });
-    } catch (err) {
-        process.exit(1); 
-    }
-}
-
-export async function disconnect() {
-    if (mongoose.connection.readyState !== 0) {
-        await mongoose.connection.close(true);
-    }
-}
+};
+   
